9dd7e1a2fe0b0dee5fdf81197fcf573429162d1c
RysKowalski <ryskowalski0@gmail.com>
2026-09-03 13:26:47 +0200

    i

--- GameEnvironment.py
+++ GameEnvironment.py
@@ -1,8 +1,24 @@
-from typing import Optional
+from typing import Any
+
 import numpy as np
 import gymnasium as gym
 
 
 class GameEnv(gym.Env):
     def __init__(self) -> None:
-        pass
+        self.map: np.ndarray = np.zeros([5, 7], dtype=np.int32)
+        self.next: np.ndarray = np.zeros([2], dtype=np.int32)
+
+        self.observation_space = gym.spaces.Dict(
+            {
+                "moves": gym.spaces.Box(low=1, high=6, shape=[2], dtype=np.int32),
+                "map": gym.spaces.Box(low=0, high=11, shape=[5, 7], dtype=np.int32),
+            }
+        )
+        self.action_space = gym.spaces.Discrete(5)
+
+    def _get_obs(self) -> dict:
+        return {"moves": self.next, "map": self.map}
+
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None):
+        return super().reset(seed=seed, options=options)
